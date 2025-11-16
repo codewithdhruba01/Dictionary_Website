cd5a5decc900e84e0dd916c6f2d70c7a92bba925
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-11-16 23:46:11 +0530

    update icon

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { BookOpen } from 'lucide-react';
+import { BookText } from 'lucide-react';
 import { FontDropdown } from './FontDropdown';
 import { ThemeToggle } from './ThemeToggle';
 
@@ -8,7 +8,7 @@ export function Navbar() {
   return (
     <nav className="w-full flex items-center justify-between py-4 sm:py-6 px-4 sm:px-6 md:px-8">
       <div className="flex items-center gap-2">
-        <BookOpen className="w-6 sm:w-8 h-6 sm:h-8 text-muted-foreground" />
+        <BookText className="w-8 sm:w-10 h-8 sm:h-10 text-muted-foreground" />
       </div>
 
       <div className="flex items-center gap-3 sm:gap-4 md:gap-6">
